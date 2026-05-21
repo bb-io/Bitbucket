bb2299d8d1df07836eb07649ca7642d145c0d71c
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-05-21 17:01:48 +0300

    add missing file data handler

--- Apps.Bitbucket/Models/Identifiers/FilePathIdentifier.cs
+++ Apps.Bitbucket/Models/Identifiers/FilePathIdentifier.cs
@@ -1,9 +1,11 @@
+using Apps.Bitbucket.Handlers;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Models.FileDataSourceItems;
 
 namespace Apps.Bitbucket.Models.Identifiers;
 
 public class FilePathIdentifier
 {
-    [Display("File path")]
+    [Display("File path"), FileDataSource(typeof(FilePickerDataHandler))]
     public string FilePath { get; set; } = string.Empty;
 }
