c6feab12c6d7e7a71ef672c1d5fa1a54ec03eb18
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-05-22 17:32:26 +0300

    fix OAuth2 AuthenticationType

--- Apps.Bitbucket/Connections/ConnectionDefinition.cs
+++ Apps.Bitbucket/Connections/ConnectionDefinition.cs
@@ -23,7 +23,7 @@ public class ConnectionDefinition : IConnectionDefinition
         {
             Name = ConnectionTypes.OAuth2,
             DisplayName = "OAuth2",
-            AuthenticationType = ConnectionAuthenticationType.OAuth2,
+            AuthenticationType = ConnectionAuthenticationType.Undefined,
             ConnectionProperties = new List<ConnectionProperty>
             {
                 new(CredsNames.ClientId) { DisplayName = "Client ID" },
