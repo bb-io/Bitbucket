7de135c51f96e98986f1647f8d5e0dec79a01dfd
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-05-22 13:10:42 +0300

    fix incorrect class passed to ff picker handler

--- Apps.Bitbucket/Models/Identifiers/Optional/OptionalFolderPathIdentifier.cs
+++ Apps.Bitbucket/Models/Identifiers/Optional/OptionalFolderPathIdentifier.cs
@@ -1,3 +1,4 @@
+using Apps.Bitbucket.Handlers.FileFolder;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.SDK.Extensions.FileManagement.Models.FileDataSourceItems;
 
@@ -5,6 +6,6 @@ namespace Apps.Bitbucket.Models.Identifiers.Optional;
 
 public class OptionalFolderPathIdentifier
 {
-    [Display("Folder path"), FileDataSource(typeof(FolderPathDataSourceContext))]
+    [Display("Folder path"), FileDataSource(typeof(FolderPickerDataHandler))]
     public string? FolderPath { get; set; }
 }
